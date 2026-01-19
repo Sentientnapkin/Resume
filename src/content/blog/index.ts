bb7edec5a798fa95e0b6a66f55e467cf5020460b
SentientNapkin <sebastian.vargas.reiners@gmail.com>
2026-01-18 23:41:00 -0800

    Added in ACT_1 blog
    Former-commit-id: 69b0e58cf87293a05c7af0b6c16b6fc3f7e9117c

--- src/content/blog/index.ts
+++ src/content/blog/index.ts
@@ -1,10 +1,16 @@
 import ImitationLearning, { frontmatter as imitationFrontmatter } from './ImitationLearning.mdx'
+import ACT_1, { frontmatter as act1Frontmatter } from './ACT_1.mdx'
 
 export const blogPosts = [
   {
     slug: 'imitation-learning',
     Component: ImitationLearning,
     ...imitationFrontmatter
+  },
+  {
+    slug: 'act-1',
+    Component: ACT_1,
+    ...act1Frontmatter
   }
 ]
 

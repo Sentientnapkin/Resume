e00caebb4e79a6d2828d8a5f2bbcb4ff4f1886f8
SentientNapkin <sebastian.vargas.reiners@gmail.com>
2026-01-20 00:00:20 -0800

    Added ACT_2 blog post

--- src/content/blog/index.ts
+++ src/content/blog/index.ts
@@ -1,7 +1,13 @@
 import ImitationLearning, { frontmatter as imitationFrontmatter } from './ImitationLearning.mdx'
 import ACT_1, { frontmatter as act1Frontmatter } from './ACT_1.mdx'
+import ACT_2, { frontmatter as act2Frontmatter } from './ACT_2.mdx'
 
 export const blogPosts = [
+  {
+    slug: 'act-2',
+    Component: ACT_2,
+    ...act2Frontmatter
+  },
   {
     slug: 'act-1',
     Component: ACT_1,
